c55dfc4fc079c5f35c12c11ac23622480061c8ab
Jorge Tricarico <jorge.tricarico@gmail.com>
2023-01-20 08:46:10 -0300

    second push

--- src/main/java/com/besysoft/bootcampspringboot/BootcampSpringBootApplication.java
+++ src/main/java/com/besysoft/bootcampspringboot/BootcampSpringBootApplication.java
@@ -11,15 +11,8 @@ import java.util.List;
 @SpringBootApplication
 public class BootcampSpringBootApplication {
 
-	@Autowired
-	static GeneradorDummyn generadorDummyn;
-	public static List<Genero> listaGeneros =  generadorDummyn.crearDatosGenero();
 	public static void main(String[] args) {
-
-
 		SpringApplication.run(BootcampSpringBootApplication.class, args);
-
-		System.out.println(listaGeneros);
 	}
 
 }
--- src/main/java/com/besysoft/bootcampspringboot/utilidades/GeneradorDummyn.java
+++ src/main/java/com/besysoft/bootcampspringboot/utilidades/GeneradorDummyn.java
@@ -2,49 +2,54 @@ package com.besysoft.bootcampspringboot.utilidades;
 
 import com.besysoft.bootcampspringboot.dominio.Genero;
 import com.besysoft.bootcampspringboot.dominio.PeliculaSerie;
+import com.besysoft.bootcampspringboot.dominio.Personaje;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GeneradorDummyn {
-        public List<Genero> crearDatosGenero (){
+
+        public static final List<Genero> listaDeGeneros = crearDatosGenero();
+
+        private static List<Genero> crearDatosGenero (){
+
 
         List<Genero> listaDeGeneros = new ArrayList<>();
 
-        List<String> listaPeliculasSerieDrama = new ArrayList<>();
-        List<String> listaPeliculasSerieAventura = new ArrayList<>();
-        List<String> listaPeliculasSerieAccion = new ArrayList<>();
-        List<String> listaPeliculasSerieTerror = new ArrayList<>();
+        List<String> listaPeliculaSerieDrama = new ArrayList<>();
+        List<String> listaPeliculaSerieAventura = new ArrayList<>();
+        List<String> listaPeliculaSerieAccion = new ArrayList<>();
+        List<String> listaPeliculaSerieTerror = new ArrayList<>();
 
-        listaPeliculasSerieDrama.add("El Temblor");
-        listaPeliculasSerieDrama.add("El Temblor 2");
-        listaPeliculasSerieDrama.add("La Esclava");
+        listaPeliculaSerieDrama.add("El Temblor");
+        listaPeliculaSerieDrama.add("El Temblor 2");
+        listaPeliculaSerieDrama.add("La Esclava");
 
-        listaPeliculasSerieAventura.add("Harry Potter 1");
-        listaPeliculasSerieAventura.add("Harry Potter 2");
-        listaPeliculasSerieAventura.add("Harry Potter 3");
-        listaPeliculasSerieAventura.add("Harry Potter 4");
-        listaPeliculasSerieAventura.add("Harry Potter 5");
+        listaPeliculaSerieAventura.add("Harry Potter 1");
+        listaPeliculaSerieAventura.add("Harry Potter 2");
+        listaPeliculaSerieAventura.add("Harry Potter 3");
+        listaPeliculaSerieAventura.add("Harry Potter 4");
+        listaPeliculaSerieAventura.add("Harry Potter 5");
 
-        listaPeliculasSerieAccion.add("Rapido y Furioso 1");
-        listaPeliculasSerieAccion.add("Rapido y Furioso 2");
-        listaPeliculasSerieAccion.add("Rapido y Furioso 3");
-        listaPeliculasSerieAccion.add("Rapido y Furioso 4");
-        listaPeliculasSerieAccion.add("Rapido y Furioso 30");
+        listaPeliculaSerieAccion.add("Rapido y Furioso 1");
+        listaPeliculaSerieAccion.add("Rapido y Furioso 2");
+        listaPeliculaSerieAccion.add("Rapido y Furioso 3");
+        listaPeliculaSerieAccion.add("Rapido y Furioso 4");
+        listaPeliculaSerieAccion.add("Rapido y Furioso 30");
 
-        listaPeliculasSerieTerror.add("La Huerfana 1");
-        listaPeliculasSerieTerror.add("La Huerfana 2");
-        listaPeliculasSerieTerror.add("La Huerfana 3");
-        listaPeliculasSerieTerror.add("La Huerfana 4");
-        listaPeliculasSerieTerror.add("Juego del Miedo 1");
-        listaPeliculasSerieTerror.add("Juego del Miedo 2");
-        listaPeliculasSerieTerror.add("Juego del Miedo 3");
-        listaPeliculasSerieTerror.add("Juego del Miedo 4");
+        listaPeliculaSerieTerror.add("La Huerfana 1");
+        listaPeliculaSerieTerror.add("La Huerfana 2");
+        listaPeliculaSerieTerror.add("La Huerfana 3");
+        listaPeliculaSerieTerror.add("La Huerfana 4");
+        listaPeliculaSerieTerror.add("Juego del Miedo 1");
+        listaPeliculaSerieTerror.add("Juego del Miedo 2");
+        listaPeliculaSerieTerror.add("Juego del Miedo 3");
+        listaPeliculaSerieTerror.add("Juego del Miedo 4");
 
-        Genero drama = new Genero("Drama", listaPeliculasSerieDrama);
-        Genero aventura = new Genero("Aventura", listaPeliculasSerieAventura);
-        Genero accion = new Genero("Accion", listaPeliculasSerieAccion);
-        Genero terror = new Genero("Terror", listaPeliculasSerieTerror);
+        Genero drama = new Genero("Drama", listaPeliculaSerieDrama);
+        Genero aventura = new Genero("Aventura", listaPeliculaSerieAventura);
+        Genero accion = new Genero("Accion", listaPeliculaSerieAccion);
+        Genero terror = new Genero("Terror", listaPeliculaSerieTerror);
 
         listaDeGeneros.add(drama);
         listaDeGeneros.add(aventura);
@@ -53,6 +58,16 @@ public class GeneradorDummyn {
 
         return listaDeGeneros;
     }
+
+        private static List<Personaje> crearPersonajes(){
+
+                List<Personaje> listaPersonajes = new ArrayList<>();
+
+
+
+
+                return listaPersonajes;
+        }
 
 
 }
